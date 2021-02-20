35fea1043d11e920b6a440705061d601485b47d6
Gunjan Mistry <gunjanmistry008@gmail.com>
2021-02-20 13:31:28 +0530

    add HotelDetail with reviews

--- app/routers/traveller/hotelDetail.py
+++ app/routers/traveller/hotelDetail.py
@@ -12,11 +12,22 @@ from ...helpers.validatation import PHONE_NUMBER_REGEX, HotelAmenitiesEnum
 router = APIRouter()
 
 
-class HotelDetailResponse(BaseModel):
+class HotelReviewsResponse(BaseModel):
+    id: str  # traveller's id
+    rating: int
+    # photos: List[AnyUrl]
+    review: str  # len 100
+    datetime: datetime
+    name: str  # traveller's name
+
+
+class HotelDetailsResponse(BaseModel):
     id: str
     photos: List[AnyUrl]
     name: str
-    location: str
+    locality: str
+    address: str
+    postalCode: str
     city: str
     rating: float
     phoneNumber: constr(min_length=13, max_length=13, regex=PHONE_NUMBER_REGEX)
@@ -27,6 +38,11 @@ class HotelDetailResponse(BaseModel):
     likes: bool
 
 
+class HotelApiResponse(BaseModel):
+    hotelDetails: HotelDetailsResponse
+    hotelReviews: List[HotelReviewsResponse]
+
+
 GET_HOTELDETAIL_QUERY = """
 MATCH
     (city:City)-[:LOCATED_IN]-(hotel:Hotel {uid:$hotel})-[review:REVIEWED_HOTEL]-()
@@ -35,7 +51,9 @@ RETURN
     hotel.photos as photos,
     hotel.name as name,
     city.name as city,
-    hotel.address+" "+hotel.locality+" "+hotel.postal_code as location,
+    hotel.locality as locality,
+    hotel.address as address,
+    hotel.postal_code as postalCode,
     AVG(review.rating) as rating,
     hotel.phone as phoneNumber,
     hotel.latitude as latitude,
@@ -44,14 +62,26 @@ RETURN
     hotel.amenities as amenities,
     EXISTS ((hotel)-[:LIKES_HOTEL]-(:User {uid:$user})) as likes
 """
+GET_HOTELDETAILREVIEW_QUERY = """
+MATCH
+    (hotel:Hotel {uid:$hotel})-[review:REVIEWED_HOTEL]-(traveller:Traveller)
+RETURN
+    traveller.uid as id,
+    review.rating as rating,
+ 	left(review.review,150) as review,
+    review.datetime as datetime,
+    traveller.name as name
+ORDER BY datetime DESC LIMIT 3
+"""
+# user=Depends(get_registered_user) TODO add back
 
-
-@router.get("", response_model=HotelDetailResponse)
-async def get_hotel_detail(id: str, user=Depends(get_registered_user)):
-    return HotelDetailResponse(
-        **(
-            get_query_response(GET_HOTELDETAIL_QUERY, {"hotel": id, "user": user.uid})[
-                0
-            ]
-        )
+# e2813cf7e74a441bbe416589b976475c user id
+@router.get("", response_model=HotelApiResponse)
+async def get_hotel_detail(id: str):
+    return HotelApiResponse(
+        hotelDetails=get_query_response(
+            GET_HOTELDETAIL_QUERY,
+            {"hotel": id, "user": "e2813cf7e74a441bbe416589b976475c"},
+        )[0],
+        hotelReviews=get_query_response(GET_HOTELDETAILREVIEW_QUERY, {"hotel": id}),
     )
