4cf6cd79b33f2d3bbf6c46d871130a9fba68d80f
Gunjan Mistry <gunjanmistry008@gmail.com>
2021-02-22 01:51:14 +0530

    add profile query :construction:

--- app/routers/traveller/__init__.py
+++ app/routers/traveller/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from . import auth, community, home
+from . import auth, community, home, profile
 
 router = APIRouter()
 
--- app/routers/traveller/profile.py
+++ app/routers/traveller/profile.py
@@ -5,23 +5,24 @@ from pydantic import AnyUrl, BaseModel, constr
 
 from ...dependencies.auth import get_registered_user
 from ...helpers.conversion import get_query_response
+from ...models.database import Traveller
 
 router = APIRouter()
 
 
-class ProfileBookingResponse(BaseModel):
+class ProfileBookingsResponse(BaseModel):
     pass
 
 
-class ProfileReviewResponse(BaseModel):
+class ProfileReviewsResponse(BaseModel):
     pass
 
 
-class ProfileBlogResponse(BaseModel):
+class ProfileBlogsResponse(BaseModel):
     pass
 
 
-class ProfileFavResponse(BaseModel):
+class ProfileFavsResponse(BaseModel):
     pass
 
 
@@ -31,7 +32,31 @@ class ProfileApiResponse(BaseModel):
     coverUri: AnyUrl
     phoneNumber: str
     name: str
-    bookings: List[ProfileBookingResponse]
-    reviews: List[ProfileReviewResponse]
-    blogs: List[ProfileBlogResponse]
-    favs: List[ProfileFavResponse]
+    bookings: List[ProfileBookingsResponse]
+    reviews: List[ProfileReviewsResponse]
+    blogs: List[ProfileBlogsResponse]
+    favs: List[ProfileFavsResponse]
+
+
+# =Depends(get_registered_user)
+@router.get("", response_model=ProfileApiResponse)
+async def get_profile_data(user: str):
+    traveller = Traveller.nodes.get(uid=user)
+    bookings = traveller.nodes.booked_hotel.all()
+    bookings += traveller.nodes.booked_package.all()
+    # print("bookings:", bookings)
+    reviews = traveller.nodes.reviewed_attraction.all()
+    reviews += traveller.nodes.reviewed_city.all()
+    reviews += traveller.nodes.reviewed_package.all()
+    reviews += traveller.nodes.reviewed_shop.all()
+    reviews += traveller.nodes.reviewed_hotel.all()
+    # print("reviews:", reviews)
+    blogs = traveller.nodes.author_of.all()
+    # print("blogs:", blogs)
+    favs = traveller.nodes.likes_hotel.all()
+    favs += traveller.nodes.likes_city.all()
+    favs += traveller.nodes.likes_package.all()
+    favs += traveller.nodes.likes_shop.all()
+    favs += traveller.nodes.likes_blog.all()
+    favs += traveller.nodes.likes_attraction.all()
+    print("favs:", favs)
