fcbbda32ed0a5f7d737603cc781ab7576a8962c9
Gunjan Mistry <gunjanmistry008@gmail.com>
2021-03-10 18:10:13 +0530

    update blog route added:busstop:

--- app/routers/traveller/blog.py
+++ app/routers/traveller/blog.py
@@ -7,8 +7,10 @@ from pydantic import AnyUrl, BaseModel
 
 from ...dependencies.auth import get_registered_user
 from ...dependencies.entities import get_blog
-from ...helpers.db_query import get_query_response
+from ...helpers.conversion import deflate_request
+from ...helpers.db_query import get_query_response, update_node
 from ...helpers.queries import GET_BLOG_COMMENTS_QUERY, GET_BLOG_DETAILS_QUERY
+from ...models.database import Blog, Traveller
 
 router = APIRouter()
 
@@ -32,6 +34,12 @@ class BlogApiResponse(BaseModel):
     likes: int
     liked: bool
     comments: List[BlogCommentResponse]
+
+
+class BlogUpdateRequest(BaseModel):
+    title: str
+    content: str
+    photos: List[AnyUrl]
 
 
 @router.get("", response_model=BlogApiResponse)
@@ -58,3 +66,15 @@ async def blog_unlike(blog=Depends(get_blog), user=Depends(get_registered_user))
     with db.transaction:
         if user.likes_blog.is_connected(blog):
             user.likes_blog.disconnect(blog)
+
+
+#
+@router.put("/update")
+async def update_blog(
+    blogDetail: BlogUpdateRequest,
+    blog=Depends(get_blog),
+    user=Depends(get_registered_user),
+):
+    # print(user.uid == blog.authored_by.single().uid)
+    if user.uid == blog.authored_by.single().uid:
+        update_node(blog, deflate_request(blogDetail, {"title", "content", "photos"}))
