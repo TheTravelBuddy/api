5b159f727f8f197f29c621074665a67f60f254ce
Gunjan Mistry <gunjanmistry008@gmail.com>
2021-03-25 18:41:33 +0530

    city nearest query

--- app/routers/traveller/city.py
+++ app/routers/traveller/city.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel, confloat
@@ -14,6 +14,12 @@ class CityApiResponse(BaseModel):
     name: str
     latitude: confloat(ge=-90, le=90)
     longitude: confloat(ge=-180, le=180)
+    dist: float
+
+
+class CityApiRequest(BaseModel):
+    latitude: confloat(ge=-90, le=90)
+    longitude: confloat(ge=-90, le=90)
 
 
 GET_ALL_CITY_QUERY = """
@@ -22,10 +28,18 @@ RETURN
     city.uid AS id,
     city.name AS name,
     city.latitude AS latitude,
-    city.longitude AS longitude
+    city.longitude AS longitude,
+    distance(point({longitude:$clong, latitude:$clat}),point({longitude:city.longitude,latitude:city.latitude})) as dist 
+order by dist
 """
 
 
 @router.get("", response_model=List[CityApiResponse])
-async def get_cities(_=Depends(get_registered_user)):
-    return get_query_response(GET_ALL_CITY_QUERY)
+async def get_cities(lat: str = "", long: str = ""):
+    if lat == "" and long == "":
+        lat = None
+        long = None
+    else:
+        lat = int(lat)
+        long = int(long)
+    return get_query_response(GET_ALL_CITY_QUERY, {"clong": long, "clat": lat})
