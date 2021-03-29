7907defc0a7efafcd94d09b9722ae5be73a99537
Divy Jain <dkj@somaiya.edu>
2021-03-30 02:35:21 +0530

    Add Profile Photo Upload Endpoint

--- app/routers/traveller/profile.py
+++ app/routers/traveller/profile.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
+from pydantic import AnyUrl, BaseModel
 
 from ...dependencies.auth import get_registered_user
 from ...helpers.conversion import deflate_request
@@ -23,3 +23,15 @@ async def edit_profile(
     userDetail: ProfileEditRequest, user=Depends(get_registered_user)
 ):
     update_node(user, deflate_request(userDetail, {"name", "gender", "dob", "mood"}))
+
+
+class ProfilePhotoEditRequest(BaseModel):
+    profilePicture: AnyUrl
+
+
+@router.put("/picture")
+async def edit_profile_photo(
+    userDetail: ProfilePhotoEditRequest, user=Depends(get_registered_user)
+):
+    user.profile_picture = str(userDetail.profilePicture)
+    user.save()
