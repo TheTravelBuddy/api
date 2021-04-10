1e597380668342978511f77deec02537276de24d
Gunjan Mistry <gunjanmistry008@gmail.com>
2021-04-11 01:09:39 +0530

    add hoteldetail route

--- app/routers/business/hotel.py
+++ app/routers/business/hotel.py
@@ -1,4 +1,5 @@
-from typing import List
+from datetime import datetime
+from typing import List, Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from neomodel import db
@@ -6,8 +7,12 @@ from pydantic import AnyUrl, BaseModel, confloat, constr
 
 from ...helpers.conversion import deflate_request
 from ...helpers.db_query import get_query_response
-from ...helpers.queries import GET_OWNED_HOTELS_QUERY
-from ...helpers.validation import HotelAmenitiesEnum
+from ...helpers.queries import (
+    GET_HOTEL_DETAILS_QUERY,
+    GET_HOTEL_REVIEWS_QUERY,
+    GET_OWNED_HOTELS_QUERY,
+)
+from ...helpers.validation import PHONE_NUMBER_REGEX, HotelAmenitiesEnum
 from ...models.database import Hotel
 from .auth import get_business
 
@@ -26,6 +31,59 @@ class NewHotelRequest(BaseModel):
     longitude: float
     phone: str
     amenities: List[HotelAmenitiesEnum]
+
+
+GET_HOTEL_DETAIL_QUERY = """
+MATCH
+    (city:City)-[:LOCATED_IN]-(hotel:Hotel {uid:$hotel})
+OPTIONAL MATCH
+    (hotel)-[review:REVIEWED_HOTEL]-()
+OPTIONAL MATCH
+	(hotel)-[likes:LIKES_HOTEL]-()
+RETURN
+    hotel.uid AS id,
+    hotel.photos AS photos,
+    hotel.name AS name,
+    city.name AS city,
+    hotel.locality AS locality,
+    hotel.address AS address,
+    hotel.postal_code AS postalCode,
+    AVG(review.rating) AS rating,
+    hotel.phone AS phoneNumber,
+    hotel.latitude AS latitude,
+    hotel.longitude AS longitude,
+    hotel.price AS price,
+    hotel.description AS about,
+    hotel.amenities AS amenities,
+    COUNT(likes) AS likes
+"""
+
+
+class HotelReviewResponse(BaseModel):
+    id: str
+    rating: int
+    review: str
+    publishedOn: datetime
+    name: str
+
+
+class HotelApiResponse(BaseModel):
+    id: str
+    photos: List[AnyUrl]
+    name: str
+    locality: str
+    address: str
+    postalCode: str
+    city: str
+    rating: Optional[float] = None
+    price: int
+    phoneNumber: constr(min_length=13, max_length=13, regex=PHONE_NUMBER_REGEX)
+    latitude: confloat(ge=-90, le=90)
+    longitude: confloat(ge=-180, le=180)
+    about: str
+    likes: int
+    amenities: List[HotelAmenitiesEnum]
+    reviews: List[HotelReviewResponse]
 
 
 # user=Depends(get_business)
@@ -56,3 +114,23 @@ async def add_hotel(hotelData: NewHotelRequest, hotelier=Depends(get_business)):
             )
         ).save()
         hotel.owned_by.connect(hotelier)
+
+
+# ,hotelier=Depends(get_business)
+@router.get("/hoteldetail")
+async def hotel_detail(hotel: str):
+    try:
+        return HotelApiResponse(
+            **get_query_response(
+                GET_HOTEL_DETAIL_QUERY,
+                {"hotel": hotel},
+            )[0],
+            reviews=get_query_response(
+                GET_HOTEL_REVIEWS_QUERY, {"hotel": hotel, "n": 3}
+            ),
+        )
+    except IndexError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Hotel not found.",
+        )
